88e4bb29a43a2ad1cbdcf2bdd61571057de280c7
turner <turner108@herren.co.kr>
2023-02-27 18:14:48 +0900

    test 및 buildspec.yml 추가

---
+++ src/test/kotlin/com/build/FailTest.kt
@@ -0,0 +1,12 @@
+package com.build
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class FailTest {
+
+    @Test
+    fun shouldTestFail() {
+        Assertions.assertTrue(false)
+    }
+}
---
+++ src/test/kotlin/com/build/SuccessTest.kt
@@ -0,0 +1,12 @@
+package com.build
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class SuccessTest {
+
+    @Test
+    fun shouldTestSuccess() {
+        Assertions.assertTrue(true)
+    }
+}
